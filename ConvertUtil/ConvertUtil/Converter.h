3e82b7ce0bc69e5e66bc5d5288cb3260c15e6ba0
Alexander Scott <xander@Alexanders-Mac.local>
2014-05-06 20:03:07 +1000

    Convert

---
+++ ConvertUtil/ConvertUtil/Converter.h
@@ -0,0 +1,11 @@
+//
+// Created by Alexander Scott on 6/05/2014.
+// Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Converter <NSObject>
+
+- (NSNumber *)convert:(NSNumber *)fromInput;
+@end
---
+++ ConvertUtil/ConvertUtil/ImperialConverter.m
@@ -0,0 +1,12 @@
+//
+// Created by Alexander Scott on 6/05/2014.
+// Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import "ImperialConverter.h"
+
+
+@implementation ImperialConverter {
+
+}
+@end
