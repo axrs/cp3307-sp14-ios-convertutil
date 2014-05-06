246e691fd5ddc1bae5e93bea6c6b1ecc678ab564
Alexander Scott <xander@Alexanders-Mac.local>
2014-05-06 19:52:59 +1000

    XCode Blank Project

---
+++ ConvertUtil/ConvertUtil/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ConvertUtil
+//
+//  Created by Alexander Scott on 6/05/2014.
+//  Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ConvertUtil/ConvertUtil/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ConvertUtil
+//
+//  Created by Alexander Scott on 6/05/2014.
+//  Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
